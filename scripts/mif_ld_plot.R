d731533840ac401fa99d647af22505e52f838750
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-08-26 13:27:19 +0200

    split scripts for LD heatmap for cs eQTLs #3

--- scripts/mif_ld_plot.R
+++ scripts/mif_ld_plot.R
@@ -1,15 +1,22 @@
 
 library(tidyverse)
+library(data.table)
+library(corrplot)
 
 
 region <- "22_24234172_24401503" # MIF
 region <- "22_23942068_26128449" # GGT1
 
-path_base <- "/scratch/dariush.ghasemi/projects/susie_pqtls/ld_block/results/"
+#--------------------------------# 
+# inputs
+path_base <- "/scratch/dariush.ghasemi/projects/pqtl_mif/"
 path_ld <- paste0(path_base, "ld/", region, ".ld")
 path_ld_matrix <- paste0(path_base, "ldmat/", region, ".ld")
 path_ld_snps <- paste0(path_base, "ldmat/", region, ".snps")
 
+# outputs
+
+#--------------------------------# 
 # Load LD matrix
 # change class to matrix
 ld_matrix <- as.matrix(data.table::fread(path_ld_matrix))
@@ -21,6 +28,9 @@ snp_positions <- read.table(path_ld_snps, col.names = c("SNP", "Position"))
 colnames(ld_matrix) <- snp_positions$Position
 rownames(ld_matrix) <- snp_positions$Position
 
+#================================#
+#----       LD corplot       ---- 
+#================================#
 
 pdf("LD_blocks_GGT1.pdf", width = 15, height = 15) #, units = "cm", res = 300
 jpeg("LD_blocks_GGT1.jpg", width = 15, height = 15, units = "cm", res = 700)
@@ -46,7 +56,7 @@ heatmap(
 dev.off()
 
 
-
+#--------------------------------# 
 
 ld_long <- data.table::fread(path_ld) #%>%
 #  as.data.frame() %>%
@@ -63,49 +73,4 @@ ld_long %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   labs(title = "LD Heatmap", x = "SNP", y = "SNP")
 
-
-
-#=============================#
-# -----    MIF index     -----
-#=============================#
-
-library(corrplot)
-
-# calculate LD between index variants of the 6 loci in broader MIF region 
-mif_index <- "/scratch/dariush.ghasemi/projects/susie_pqtls/ld_block/results/dosage/mif_cojo.raw"
-
-#---- Read genotype file ----
-genos <- fread(mif_index)
-
-# remove effective allele from column names
-genos <- genos %>% rename_with(~ str_remove(., "_[ATCG]+$"), matches("_[ATCG]+$"))
-
-# store variants list for the match
-snps <- names(genos)[7:ncol(genos)]
-
-X <- as.matrix(genos[,7:ncol(genos)]) * 1.0
-
-# compute LD (r2)
-R <- cor(X, use = "complete.obs")  # ---> Find a solution for NAs in genotype and cor matrix
-
-
-corrplot.mixed(R)
-
-corrplot(
-  R, 
-  method = 'number', 
-  diag = FALSE,
-  #type = 'upper',
-  col = COL2('PRGn') #COL2('BrBG')
-  ) # colorful number
-
-
-
-# Gene is used for annotation of Pheatmap
-annot_heatmap <- data.frame(
-  row.names = colnames(R),
-  Gene = c("MIF", "MIF", "DDT", "GSTT1", "GSTT1", "DDT", "GSTT1", "GGT5", "GGT5", "GGT5", "F10")
-)
-
-pheatmap(R, annotation_col = annot_heatmap)
 
